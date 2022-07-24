e7e58cca4c174a4cc4d5ec8fd42f3ea5aa1bad95
MateuszKlepacki1 <mateusz.klepacki@gmail.com>
2022-07-24 11:08:51 +0200

    Stworzenie kalkulatora z opcją dodawania

---
+++ Kalkulator.java
@@ -0,0 +1,12 @@
+public class Kalkulator {
+/*
+1. dodawanie
+2. mnożenie
+3. odejmowanie
+ */
+public int  add (int a, int b){
+    int c;
+    c = a + b;
+    return c;
+}
+}
---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/*public class Main {
+    int a;
+    int b;
+    a = 10;
+    b = 2;
+    int c;
+    c = a - b;
+    System.out.println(c);
+
+}
+ */
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Kalkulator calc = new Kalkulator();
+        calc.add(a:5,b:2)
+        System.out.println(calc.add(a,b));
+
+         */
+        Object system = null;
+        Kalkulator calc = new Kalkulator();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Prosze podac pierwsza liczbe");
+        int firstVariable = scanner.nextInt();
+        System.out.println("Prosze podac druga liczbe");
+        int secondVariable = scanner.nextInt();
+        System.out.println("Twoj wynik to " +calc.add(firstVariable, secondVariable));
+
+
+    }
+}
+
